3119cea2a61f5b7c6acacbabdb13894d3ec4b95a
Sutanissaya <167442356+sutanissaya@users.noreply.github.com>
2024-05-02 15:40:03 +0700

    Update revisepalitext.py

--- revisepalitext.py
+++ revisepalitext.py
@@ -73,12 +73,8 @@ def animation_demo() -> None:
     # Split text in UPPERCASE
     uppercase_check = False
     # Transliteration Standard Conversion
-    translit_select = st.sidebar.selectbox(
-    "Convert transliteration standard to:",
-    ("Do not convert", "IAST / International Alphabet of Sanskrit Transliteration (1894) & ALA-LC / American Library Association – Library of Congress (2012)", "ISO 15919: Pāḷi (2001)", "Velthuis (1983)"),
-    index=None,
-    placeholder="None",
-    )     
+    translit_select = ("Do not convert")
+    
     # Nasal ññ → nñ
     nñ_check = st.sidebar.checkbox(label='ññ → nñ')
     # Nasal ṅ → ng
